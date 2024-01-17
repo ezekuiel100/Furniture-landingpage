8a8510d28df3e6509396cdc5d3d3001f869482a9
unknown <ezekuiel100@gmail.com>
2024-01-17 16:11:01 -0300

    Responsive

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+let spy = new Gumshoe(".nav-links a");
+
 let nav = document.querySelector("nav");
+let burguerMenu = document.querySelector(".burguer-menu");
+
+let burguerMenuOpen = document.querySelector(".burguer-menu-open");
+let burguerMenuClose = document.querySelector(".burguer-menu-close");
+
+let navLinks = document.querySelector(".nav-links");
 
 window.addEventListener("scroll", () => {
   let scrollY = window.scrollY;
 
   if (scrollY > 2) {
     nav.classList.add("sticky");
-    console.log(scrollY);
   }
 
   if (scrollY <= 2) {
     nav.classList.remove("sticky");
   }
+});
+
+burguerMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  burguerMenuOpen.classList.toggle("open");
+  burguerMenuClose.classList.toggle("close");
 });
